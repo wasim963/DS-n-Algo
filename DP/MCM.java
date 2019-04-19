924e6410119e1393e2f8416e68240d68fbfb1d38
wasimCasper <wasimcasper17@gmail.com>
2019-04-19 05:38:49 +0530

    MCM>java modified

--- DP/MCM.java
+++ DP/MCM.java
@@ -4,6 +4,7 @@ public class MCM {
 		long start = System.currentTimeMillis();
 		int[] arr = {1, 2, 3, 4, 5 };
 		System.out.println(MCMTD(arr, 0, arr.length - 1, new int[arr.length][arr.length]));
+		System.out.println(MCMBU(arr));
 		long end = System.currentTimeMillis();
 		System.out.println(end - start);
 	}
@@ -30,6 +31,32 @@ public class MCM {
 		}
 		strg[si][ei] = min;
 		return min;
+	}
+	
+	public static int MCMBU(int[] arr) {
+		int n = arr.length;
+		int[][] strg = new int[n][n];
+
+		for (int slide = 2; slide <= n - 1; slide++) {
+			for (int si = 0; si <= n - slide - 1; si++) {
+				int ei = si + slide;
+
+				int min = Integer.MAX_VALUE;
+				for (int k = si + 1; k < ei; k++) {
+					int fh = strg[si][k];
+					int sh = strg[k][ei];
+					int sw = arr[si] * arr[k] * arr[ei];
+
+					int ta = fh + sh + sw;
+					if (ta < min) {
+						min = ta;
+					}
+				}
+				strg[si][ei] = min;
+
+			}
+		}
+		return strg[0][n - 1];
 	}
 
 }
