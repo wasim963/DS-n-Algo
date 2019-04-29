c52c86ecb253613a4efc16f389efb4dac2e3516e
wasimCasper <wasimcasper17@gmail.com>
2019-04-30 00:17:30 +0530

    CoinChange.java modified

--- DP/CoinChange.java
+++ DP/CoinChange.java
@@ -3,6 +3,7 @@ public class CoinChange {
 	public static void main(String[] args) {
 		int[] arr = { 2, 5, 3, 6 };
 		System.out.println(coinChangeTD(arr, 0, 10, new int[arr.length][11]));
+		System.out.println(coinChangeBU(arr, 10));
 	}
 
 	private static int coinChangeTD(int[] arr, int idx, int amount, int[][] strg) {
@@ -18,7 +19,17 @@ public class CoinChange {
 		int inc = coinChangeTD(arr, idx, amount - arr[idx], strg);
 		strg[idx][amount] = inc + exc;
 		return inc + exc;
+	}
 
+	private static int coinChangeBU(int[] arr, int amount) {
+		int[] table = new int[amount + 1];
+		table[0] = 1;
+		for (int coin : arr) {
+			for (int i = coin; i <= amount; i++) {
+				table[i] += table[i - coin];
+			}
+		}
+		return table[amount];
 	}
 
 }
